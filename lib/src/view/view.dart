868602c0a0ef900854db95516d6f16fb151b2ee0
Sofia <Sofia@LAPTOP-RHAU27CB>
2021-01-05 13:21:59 +0100

    added dart files to view.dart

--- lib/src/view/view.dart
+++ lib/src/view/view.dart
@@ -7,3 +7,5 @@ export 'package:timetracker_flutter/src/view/tag_card_view.dart';
 export 'package:timetracker_flutter/src/view/activity_card_view.dart';
 export 'package:timetracker_flutter/src/view/list_activities_view.dart';
 export 'package:timetracker_flutter/src/view/list_interval_view.dart';
+export 'package:timetracker_flutter/src/view/dialog_time_information.dart';
+export 'package:timetracker_flutter/src/view/dialog_create_tracker.dart';
