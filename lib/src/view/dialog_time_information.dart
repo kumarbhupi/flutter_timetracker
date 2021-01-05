7249042a467ef76bdc0fcc67fc83bc4757aea06a
Sofia <Sofia@LAPTOP-RHAU27CB>
2021-01-05 13:07:40 +0100

    created Dialog type Time Information.

---
+++ lib/src/view/dialog_time_information.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InformationTimeDialog extends StatelessWidget {
+  bool checkboxval = true;
+
+  @override
+  Widget build(BuildContext context) {
+    Future<void> _showMyDialog() async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: true, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Introduce the following information'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text('Start Time: '),
+                  ),
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'insert start time'),
+                      )),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text('End Time: '),
+                  ),
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'insert end time'),
+                      )),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: checkboxval,
+                        onChanged: (bool value) {
+                          print(value);
+                        },
+                      ),
+                      Text('All'),
+                    ],
+                  ),
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'insert tracker name'),
+                      )),
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cancel '),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(' Accept'),
+                      )
+                    ],
+                  )
+                ], //children
+              ),
+            ),
+          );
+        },
+      );
+    }
+  }
+}
